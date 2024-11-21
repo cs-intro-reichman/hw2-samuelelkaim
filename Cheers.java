ff882c6b73becafdddc352af86344119666d5383
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-21 19:56:49 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,13 @@
-
-            public class Cheers {
+public class Cheers {
 
     public static void main(String[] args) {
+        // Vérifie si les arguments sont corrects
         if (args.length != 2) {
-            System.out.println("Usage: Cheers <word> <repetitions>");
+            System.out.println("Usage: java Cheers <word> <repetitions>");
             return;
         }
 
-        String word = args[0];
+        String word = args[0].toUpperCase(); // Convertit le mot en majuscules
         int repetitions;
 
         try {
@@ -17,25 +17,13 @@
             return;
         }
 
-        // Process each letter of the word
+        // Parcourt chaque lettre du mot
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
 
-            // Use "an" for vowels and "a" for consonants
-            if ("AEIOUaeiou".indexOf(letter) != -1) {
-                System.out.println("Give me an " + letter + ": " + letter + "!");
-            } else {
-                System.out.println("Give me a " + letter + ": " + letter + "!");
-            }
-        }
+            // Utilise "an" pour les voyelles, sinon "a"
+            if (isVowel(letter)) {
+                System.out.println("Give me an " + letter +
 
-        // Print the spelled word
-        System.out.println("What does that spell?");
-        System.out.println(word + "!!!");
+       
 
-        // Repeat the word the given number of times
-        for (int i = 0; i < repetitions; i++) {
-            System.out.println(word + "!!!");
-        }
-    }
-}
