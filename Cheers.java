6260d97fffc9137124850210410f9d43a9010480
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-21 19:21:53 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        String word = args[0];
+        int counter = Integer.parseInt(args[1]);
+        String vowels = "AEFHILMNORSX";
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (letter >= 'a' && letter <= 'z') {
+                letter = (char) (letter - 'a' + 'A');
+            }
+
+            if (vowels.indexOf(letter) != -1) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+        for (int i = 0; i < counter; i++) {
+            System.out.println(word.toUpperCase() + "!!!");
+        }
+    }
 }
