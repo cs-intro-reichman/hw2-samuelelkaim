28148f801787f51f2720cb4cfcf340389bd5bf4a
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-21 19:39:15 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -18,9 +18,23 @@ public class Cheers {
             return;
         }
 
-        // Boucle pour afficher le mot avec un point d'exclamation
+        // Épelle chaque lettre du mot
+        for (int i = 0; i < mot.length(); i++) {
+            char lettre = mot.charAt(i);
+            if ("AEIOUYaeiouy".indexOf(lettre) != -1) {
+                System.out.println("Give me an " + lettre + ": " + lettre + "!");
+            } else {
+                System.out.println("Give me a " + lettre + ": " + lettre + "!");
+            }
+        }
+
+        // Message après avoir épelé le mot
+        System.out.println("What does that spell?");
+        System.out.println(mot + "!!!");
+
+        // Répète le mot le nombre de fois demandé
         for (int i = 0; i < repetitions; i++) {
-            System.out.println(mot + "!");
+            System.out.println(mot + "!!!");
         }
     }
 }
